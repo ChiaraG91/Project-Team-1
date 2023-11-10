a722eef4a9a22c21bfe4eace8c6f1e8b962f6917
Roccaldo <roccaldodigrisolo@gmail.com>
2023-11-10 17:35:12 +0100

    add roccaldo digrisolo

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args){
 
-        String[] nomeCognomeStudenti = new String[6]
+        String[] nomeCognomeStudenti = new String[6];
+        System.out.println("Roccaldo Digrisolo");
     }
 
 
