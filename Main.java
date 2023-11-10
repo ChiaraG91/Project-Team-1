2c5fea7ec28c8cbd9ee66422c0aaecb01f97fb17
Keoni03PA <palumbo.g.manuel@gmail.com>
2023-11-10 17:51:09 +0100

    aggiunto print name

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String[] nomeCognomeStudenti = new String[6];
         System.out.println("Roccaldo Digrisolo");
+        System.out.println("Giovanni Manuel Palumbo");
+
     }
-
-
-
 }
