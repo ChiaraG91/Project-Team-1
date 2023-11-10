a0bdd34ec39fabd41e4269972849410f30a38ec0
Roccaldo <roccaldodigrisolo@gmail.com>
2023-11-10 18:30:02 +0100

    fix all

--- Main.java
+++ Main.java
@@ -1,13 +1,8 @@
 public class Main {
-
     public static void main(String[] args){
-
         String[] nomeCognomeStudenti = new String[6];
         System.out.println("Roccaldo Digrisolo");
         System.out.println("Ubaldo Capalbo");
         nomeCognomeStudenti[1] = "Agostino Arcadi";
     }
-
-
-
 }
