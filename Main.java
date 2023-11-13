1b9a5bcd56c5d8d3fc8bd012da76f8ae6b6220a7
gianluca <alex992palermo@gmail.com>
2023-11-13 10:34:17 +0100

    Aggiunto nome e cognome

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Ubaldo Capalbo");
         nomeCognomeStudenti[1] = "Agostino Arcadi";
         System.out.println("Chiara Giovoni");
+        System.out.println("Gianluca Mulieddi");
     }
 }
