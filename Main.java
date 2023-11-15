0b81344be76f6bd674d02f90c89fddc474152e6f
Roccaldo <roccaldodigrisolo@gmail.com>
2023-11-15 15:04:02 +0100

    AddArrayRoccaldo

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         nomeCognomeStudenti[1] = "Agostino Arcadi";
         System.out.println("Chiara Giovoni");
         nomeCognomeStudenti[0] = "Chiara Giovoni";
-
+        nomeCognomeStudenti[3] = "Roccaldo digrisolo";
         for (String nomiCognomi : nomeCognomeStudenti) {
             System.out.println(nomiCognomi);
         }
